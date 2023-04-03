d9237dae90c3c585123bbc068d6c46fef4485c83
qazwaz <edwardtuzimek@gmail.com>
2023-04-03 17:36:06 +0200

    Rules

--- src/pages/Rules.js
+++ src/pages/Rules.js
@@ -1,9 +1,103 @@
-import React from 'react'
+import React from "react";
+import "../App.css";
 
-function Rules() {
+export default function Rules() {
   return (
-    <div>Rules</div>
-  )
+    <>
+      <div className="regulamin">
+        <h2>Regulamin</h2>
+        <p>
+          1.
+          <br /> Warunkiem rezerwacji jest dokonanie przedpłaty w ciągu 48
+          godzin od otrzymanie maila z potwierdzeniem rezerwacji. W przypadku
+          rezygnacji z rezerwacji wpłacony zadatek nie podlega zwrotowi.
+          <br /> 2. <br />
+          Wydawanie klucza odbywa się w godzinach od 14.00 do 22.00 Klient
+          zobowiązany jest do poinformowania personelu najpóźniej dzień przed
+          przyjazdem o planowanej godzinie odbioru kluczy pod nr telefonu
+          podanym na stronie internetowej. Nietypowe godziny należy ustalać
+          indywidualnie, w szczególnych sytuacjach może to się wiązać z
+          dodatkowa opłatą.
+          <br /> 3.
+          <br />
+          Opuszczenie mieszkania odbywa się w godzinach do 10.00 rano. Dzień
+          wcześniej Klient zobowiązany jest do poinformowania personelu o
+          planowanej godzinie opuszczenia apartamentu i umówienia się na
+          przekazanie lokalu. Nietypowe godziny należy ustalać indywidualnie, w
+          szczególnych sytuacjach może to się wiązać z dodatkowa opłatą.
+          <br /> 4. <br />
+          Cena podana w e-mailu potwierdzającym rezerwację jest ceną wynajmu.
+          Ceny podane na stronie mają charakter dynamiczny uzależnione są np. od
+          pory roku i nie stanowią oferty handlowej w rozumieniu Art. 66 par.1
+          Kodeksu Cywilnego.
+          <br /> 5. <br />
+          Cena podana w e-mailu potwierdzającym rezerwację jest ceną wynajmu,
+          która uwzględnia opłaty za media oraz sprzątanie końcowe.
+          <br /> 6. <br />W czasie zakwaterowania gość zobowiązany jest do
+          okazania ważnego dowodu tożsamości, uregulowania gotówką reszty
+          należności i przekazania kaucji, podpisania umowy.
+          <br /> 7. <br />W lokalach nie jest dozwolony pobyt większej ilości
+          osób niż ilość podana w formularzu rezerwacyjnym. W lokalach
+          obowiązuje zakaz organizowania zgromadzeń i imprez. W przypadku
+          złamania powyższego zakazu, Gość jest zobowiązany do zapłacenia kary
+          umownej w wysokości 500 zł za każde naruszenie.
+          <br /> 8.
+          <br />
+          Po wcześniejszym ustaleniu istnieje możliwość bezpłatnego dostawienia
+          łóżeczka lub materaca, krzesełka do karmienia oraz wanienki do kąpieli
+          dzieci.
+          <br /> 9. <br />
+          Pobyt zwierząt ustalany jest indywidualnie. Należy poinformować o nim
+          w czasie dokonywania rezerwacji.
+          <br /> 10. <br />
+          We wszystkich mieszkaniach obowiązuje cisza nocna między godziną 22:00
+          a 06:00. Pod rygorem utraty kaucji i wezwania policji.
+          <br /> 11. <br />
+          Zabrania się palenia papierosów w lokalu i na klatce schodowej. W
+          przypadku złamania powyższego zakazu, Gość jest zobowiązany do
+          zapłacenia kary umownej w wysokości 500 zł za każde naruszenie.
+          <br /> 12. <br />
+          Goście i Osoby towarzyszące, przebywające w Apartamencie, są
+          zobowiązane do przestrzegania niniejszego Regulaminu, Ciszy nocnej,
+          zasad BHP oraz przepisów ochrony przeciwpożarowej, a także zachowania
+          zgodnego zasadami kultury osobistej.
+          <br /> 13. <br />
+          Zabrania się używania świeczek, kadzideł i otwartego ognia.
+          <br /> 14. <br />W lokalach pobierana jest kaucja w wysokości
+          300-500zł. Zgodnie z informacją podaną w mailu rezerwacyjnym.
+          <br /> 15. <br />
+          Klient odpowiedzialny jest finansowo za spowodowanie zniszczeń w
+          lokalu przez siebie i odwiedzających go osób.
+          <br /> 16. <br />
+          Klient zobowiązuje się do eksploatacji lokalu zgodnie z jego
+          przeznaczeniem, normalnego jego użytkowania i oddanie go w stanie
+          niepogorszonym. np. Zabronione jest krojenie art. spożywczych
+          bezpośrednio na blatach. Prosimy używać desek do krojenia np.
+          Korzystając z piekarnika i mikrofalówki prosimy używać naczyń z
+          pokrywkami, foli aluminiowej.
+          <br /> 17. <br />
+          Istnieje możliwość zamówienia dodatkowego sprzątania w cenie 150-200
+          zł.
+          <br /> 18. <br />
+          Nie ponosimy odpowiedzialności za rzeczy wartościowe pozostawione w
+          apartamencie.
+          <br />
+          19. <br />
+          Dla dobra wspólnego zastrzegamy sobie prawo podjęcia decyzji o nie
+          wydaniu kluczy osobom pod wpływem alkoholu, środków odurzających,
+          zachowujących się agresywnie, a tym samym stwarzających bezpośrednie
+          zagrożenie dla innych osób oraz mienia.
+          <br /> 20. <br />
+          Dane osobowe klientów są chronione zgodnie z Ustawą z dnia 29.08.1997
+          r. „O ochronie danych osobowych” (Dz.U. Nr 133 poz. 883) w sposób
+          uniemożliwiający dostęp do nich osobom trzecim Administratorem Twoich
+          danych osobowych jest: Firma FLOU, 80-536 Gdańsk, ul. Letnicka 2
+          lok.39, NIP:9121705336, Regon 520870738.
+          <br /> 21. <br />
+          Rozstrzyganie sporów należy do sądu rzeczowego właściwego dla miasta
+          Gdańsk. Dziękujemy i życzymy udanego urlopu!
+        </p>
+      </div>
+    </>
+  );
 }
-
-export default Rules
